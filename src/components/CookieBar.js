d25e7d9cf775db23a331e211b2aea676e87091c7
katja-droid <sosnyukk@gmail.com>
2024-06-21 23:42:51 +0300

    cookiefix

--- src/components/CookieBar.js
+++ src/components/CookieBar.js
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import styles from './CookieBar.module.css';
+import { Link } from 'react-router-dom';
 
 const CookieBar = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -21,7 +22,7 @@ const CookieBar = () => {
   return (
     isVisible && (
       <div className={styles.cookieBar}>
-        <p>This website uses cookies. Press the button to confirm you agree to this. <a href="/privacy">Learn more</a></p>
+        <p>This website uses cookies. Press the button to confirm you agree to this. <Link to="/privacy">Learn more</Link></p>
         <button onClick={handleConfirm} className={styles.confirmButton}>Confirm</button>
       </div>
     )
